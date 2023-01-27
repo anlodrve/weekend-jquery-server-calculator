f60528b9be48bda3e01c8d4433803ddffd3bb811
Andrea Love <andrealovelovelove@gmail.com>
2023-01-27 17:43:25 -0600

    set up form inputs

---
+++ public/client.js
@@ -0,0 +1,18 @@
+$(document).ready(onReady);
+
+function onReady() {
+    // buttons
+        $('#additionButton').on('click', sendData);
+        $('#subtractionButton').on('click', sendData);
+        $('#multiplicationButton').on('click', sendData);
+        $('#divisionButton').on('click', sendData); 
+
+}
+let arrayCS = [];
+
+function sendMath(event) {
+    
+
+}
+// handle get requests 
+
---
+++ public/server.js
@@ -0,0 +1,3 @@
+// logic for the calculator must be located on the server
+// takes two input values and puts them into a mathematical equation 
+// 
