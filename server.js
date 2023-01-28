576763ac285ecf379ff3e3f3f5fbdfd3a2fa2801
Andrea Love <andrealovelovelove@gmail.com>
2023-01-27 18:24:51 -0600

    basic set up for post on client and server side

--- server.js
+++ server.js
@@ -1,3 +1,29 @@
 // logic for the calculator must be located on the server
+
+// here are the global variables
+const express = require('express');
+const app = express(); 
+const PORT = 5000;
+const arraySS = [];
+
+// This must be added before GET & POST routes.
+app.use(bodyParser.urlencoded({extended:true}))
+// static files (HTML, CSS, Client JS)
+app.use(express.static('server/public'));
+
+app.listen(PORT, () => {
+    console.log ('Server is running on port', PORT)
+  })
+
+  //initial server receiving the post
+app.post('/calculation', (request, response) => {
+    let mathObjectSS = request.body;
+    arraySS.push(mathObjectSS); 
+
+    response.sendStatus(201);
+})
+
+
+
 // takes two input values and puts them into a mathematical equation 
 // 
